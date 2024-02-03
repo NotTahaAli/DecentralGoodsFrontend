8aafc2097b2e966de4a7677aa04127d199a39a86
m-tahaali <tahaali.6074+github@gmail.com>
2024-02-03 21:24:11 +0500

    Favicon.ico removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,16 @@ import "./globals.css";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { Providers } from "@/components/providers";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DecentralGoods",
   description: "Decentralized Marketplace to sell your items. End to End Encryption to protect your data. No fees. No middleman. No tracking. No ads. No data collection. No cookies. No tracking. No analytics.",
+  icons: {
+    icon: "/img/logo.svg",
+  }
 };
 
 export default function RootLayout({
